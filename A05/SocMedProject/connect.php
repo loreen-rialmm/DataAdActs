dfffdf39280285dcdc727b74344430e2fc112abd
Loreen Marajas <marajasmaloreen@gmail.com>
2024-11-17 11:49:06 +0800

    Update connect.php

--- A05/SocMedProject/connect.php
+++ A05/SocMedProject/connect.php
@@ -2,7 +2,7 @@
   $dbhost = "localhost";
 	$dbuser = "root";
 	$dbpass = "";
-	$db = "system_db";
+	$db = "database_system";
 
 	$conn = new mysqli($dbhost, $dbuser, $dbpass,$db) or die("Connect failed: %s\n". $conn -> error);
 
